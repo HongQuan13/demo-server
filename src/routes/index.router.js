742d831ab184604981065a98cf814722d9ca0732
Quân Nguyễn Hồng <benjamin_nguyen@Quans-MacBook-Air-2.local>
2024-02-23 22:41:54 +0800

    fix import pool

--- src/routes/index.router.js
+++ src/routes/index.router.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const pool = require("../databases/init.postgres");
 
 router.get("/checkstatus", (req, res, next) => {
   res.status(200).json({
