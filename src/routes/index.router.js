b778d6382d58449b49059ff59176d24102d67611
Quân Nguyễn Hồng <benjamin_nguyen@Quans-MacBook-Air-2.local>
2024-02-23 21:06:06 +0800

    add api users

--- src/routes/index.router.js
+++ src/routes/index.router.js
@@ -1,11 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/checkstatus', (req, res, next) => {
-    res.status(200).json({
-        status: 'success',
-        message: 'api ok'
-    })
-})
+router.get("/checkstatus", (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    message: "api ok",
+  });
+});
+
+router.get("/api/users", (req, res, next) => {
+  res.status(200).json({
+    status: "success api",
+    message: "api ok",
+    metadata: [
+      {
+        name: "Benjamin",
+        age: 21,
+      },
+      {
+        name: "Hanry",
+        age: 30,
+      },
+      {
+        name: "Linda",
+        age: 16,
+      },
+    ],
+  });
+});
 
 module.exports = router;
